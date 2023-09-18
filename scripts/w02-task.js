bc36d3778231a1800271f3d708225db72a866a11
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-09-18 14:34:12 -0600

    W02: Programming Task

---
+++ scripts/w02-task.js
@@ -0,0 +1,42 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Craig Freeze";
+let currentYear = 2023;
+let profilePicture = "images/craig-freeze-profile.png";
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile Image of ${fullName}`);
+
+
+
+
+/* Step 5 - Array */
+favoriteFoods = ["Orange Chicken", "Steak", "Honey Bunches of Oats"];
+foodElement.textContent = favoriteFoods;
+addFavFood = "Chips and Salsa";
+favoriteFoods.push(addFavFood);
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+
+
+
+
+
+
