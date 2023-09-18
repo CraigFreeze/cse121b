77c6d8159ce3a875eec792c3d7fb35433e4b6405
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-09-18 13:53:32 -0600

    dom-basics.html and js

---
+++ dom-basics.js
@@ -0,0 +1,16 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImg = document.createElement("img");
+newImg.setAttribute("src", "https://picsum.photos/200");
+newImg.setAttribute("alt", "Randomly generated image");
+document.body.appendChild(newImg);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>CSE 121b</h2> <p>Welcome to Javascript Language</p>"
+document.body.appendChild(newSection);
